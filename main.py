a033e47b197f60cc4c2c8fa6d4092400a5e39ccc
Endybits <endyb.dev@gmail.com>
2024-02-15 12:22:10 +1100

    implement dotenv

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    print(api_key)
     return {"Hello": "World"}
